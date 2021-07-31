a5fa23c7728acb963478dea150990e3ea0e1654e
gautamchawlacr7 <gautamchawla1999@gmail.com>
2021-08-01 00:29:59 +0530

    add test for divisible by 3

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Module Docstring
+"""
+
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    # Main entry point of the app
+    print("what's popping? i am a fun converter. I convert things to FooBarQix")
+
+
+def fooBarQix(input):
+
+    return 0
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
---
+++ tests.py
@@ -0,0 +1,23 @@
+"""
+This file demonstrates common uses for the Python unittest module
+https://docs.python.org/3/library/unittest.html
+"""
+import random
+import unittest
+from main import fooBarQix
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    """ This is one of potentially many TestCases """
+
+    def setUp(self):
+        self.seq = list(range(10))
+
+    def test_zero(self):
+        self.assertEqual(0, fooBarQix(0))
+
+    def test_divisible_by_three(self):
+        self.assertEqual("Foo", fooBarQix(6))
+
+if __name__ == '__main__':
+    unittest.main()
